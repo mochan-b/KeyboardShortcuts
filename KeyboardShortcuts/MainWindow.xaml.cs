e8b0eda1a83e69c76545174f9a1902124ee95d4b
Mochan Shrestha <mochan.shrestha@gmail.com>
2018-02-06 01:39:06 -0500

    Visual Studio project and Readme file

---
+++ KeyboardShortcuts/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KeyboardShortcuts
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RunTest(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Test!");
+        }
+    }
+
+    public static class Command
+    {
+
+        public static readonly RoutedUICommand DoSomething = new RoutedUICommand("Do something", "DoSomething", typeof(MainWindow));
+
+    }
+}
